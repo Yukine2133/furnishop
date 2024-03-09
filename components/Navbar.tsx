14bf3f9af72ae1f0656fb115929f30f55fe9850f
Yukine213 <kondratenkov1109@gmail.com>
2024-03-09 19:12:35 +0200

    add mobile navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <nav className="pt-7 px-6 lg:px-24 xl:px-[8.125rem] flex justify-between text-white ">
       <h1 className="text-2xl lg:text-4xl font-bold">FurniShop</h1>
@@ -10,9 +19,19 @@ const Navbar = () => {
         <li>Features</li>
         <li>Contact</li>
       </ul>
-      <button className="md:hidden">
+      <button onClick={toggleDropdown} className="md:hidden relative">
         <Image src="/menu.svg" alt="Menu" width={24} height={24} />
       </button>
+
+      {/* Mobile Navigation */}
+      {isMenuOpen && (
+        <ul className="absolute right-4 space-y-4 top-16">
+          <li className="font-semibold">Home</li>
+          <li>About</li>
+          <li>Features</li>
+          <li>Contact</li>
+        </ul>
+      )}
     </nav>
   );
 };
